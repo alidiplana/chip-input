6ef75ac083d1066472992a51321cb72bbe93e8c9
Muhammad Ali <alidiplana@gmail.com>
2021-10-04 15:19:14 +0500

    styling + error handling

--- src/components/ChipInput.js
+++ src/components/ChipInput.js
@@ -4,20 +4,29 @@ import { useState } from "react";
 import CustomTag from "./chipInput/tag";
 import "./chipInput.scss";
 import { ErrorMessage, Field } from "formik";
+import { Tooltip } from "antd";
 
 const ChipInput = (props) => {
   const { name } = props;
   // const [isDelete, setIsDelete] = useState(false);
   const [email, setEmail] = useState();
 
+  const [enter, setEnter] = useState(false);
+
   const validate = (value) => {
-    console.log("calling");
     let errorMessage;
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
       errorMessage = "Invalid email address";
     }
     return errorMessage;
   };
+
+  // const showErrorMessage = (errorMessage) => {
+  //   console.log("calling");
+  // };
+  // const text = <span>{"not email"}</span>;
+
+  console.log("enter: ", enter);
 
   return (
     <Field name={name}>
@@ -42,43 +51,49 @@ const ChipInput = (props) => {
                 />
               );
             })}
-            <input
-              placeholder="Enter more emails"
-              className="input"
-              {...props}
-              name={name}
-              value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-              onKeyDown={(e) => {
-                if (
-                  e.key === "Backspace" &&
-                  email.length === 0 &&
-                  value.length > 0
-                ) {
-                  value.pop();
-                  setFieldValue("email", value);
-                }
-                // console.log("e ", e.key);
-              }}
-              onKeyPress={(e) => {
-                // console.log("e.key: ", e);
-                if (e.key === "Enter") {
-                  if (validate(email)) {
-                    const errorMessage = validate(email);
-                    console.log("errorMessage: ", errorMessage);
-                    <ErrorMessage name="email">{errorMessage}</ErrorMessage>;
-                    return;
-                  }
-                  if (email.length > 0) {
-                    value.push(email);
+            <Tooltip visible={enter} placement="bottom" title={validate(email)}>
+              <input
+                placeholder="Enter more emails"
+                className="input"
+                {...props}
+                name={name}
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+                onKeyDown={(e) => {
+                  if (
+                    e.key === "Backspace" &&
+                    email.length === 0 &&
+                    value.length > 0
+                  ) {
+                    value.pop();
                     setFieldValue("email", value);
                   }
-                  setEmail("");
-                }
-              }}
-            />
+                  // console.log("e ", e.key);
+                }}
+                onKeyPress={(e) => {
+                  // console.log("e.key: ", e);
+                  setEnter(false);
+                  if (e.key === "Enter") {
+                    if (validate(email)) {
+                      setEnter(true);
+                      // const errorMessage = validate(email);
+                      // console.log("errorMessage: ", errorMessage);
+                      // <ErrorMessage name="email">{errorMessage}</ErrorMessage>;
+                      // showErrorMessage(errorMessage);
+
+                      return;
+                    }
+                    if (email.length > 0) {
+                      value.push(email);
+                      setFieldValue("email", value);
+                    }
+                    setEmail("");
+                  }
+                }}
+              />
+            </Tooltip>
           </section>
         );
       }}
